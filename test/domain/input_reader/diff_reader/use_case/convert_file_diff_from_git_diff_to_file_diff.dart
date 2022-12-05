3b48731d94967daa7dd977b2a8c1796398b6a920
Barreto <barreto.tales@gmail.com>
2022-12-05 12:16:59 -0300

    fix coverage diff wrong line

--- test/domain/input_reader/diff_reader/use_case/convert_file_diff_from_git_diff_to_file_diff.dart
+++ test/domain/input_reader/diff_reader/use_case/convert_file_diff_from_git_diff_to_file_diff.dart
@@ -2,9 +2,9 @@ import 'package:pull_request_coverage/domain/input_reader/diff_reader/use_case/c
 import 'package:test/test.dart';
 
 void main() {
-  final fileLines = _fileContent.split("\n");
 
-  group("When file content is mapped", () {
+  group("When file1 content is mapped", () {
+    final fileLines = _file1Content.split("\n");
     final useCase = ConvertFileDiffFromGitDiffToFileDiff();
 
     final result = useCase(fileLines)!;
@@ -19,9 +19,22 @@ void main() {
       () => expect(result.lines.firstWhere((element) => element.lineNumber == 55).line, "+    image: cirrusci/flutter:3.3.7"),
     );
   });
+
+  // bugfix
+  group("When file2 content is mapped", () {
+    final fileLines = _file2Content.split("\n");
+    final useCase = ConvertFileDiffFromGitDiffToFileDiff();
+
+    final result = useCase(fileLines)!;
+
+    test(
+      "expect that line 17 is `     commands:`",
+      () => expect(result.lines.firstWhere((element) => element.lineNumber == 2).line, "+import 'package:flutter_svg/flutter_svg.dart';"),
+    );
+  });
 }
 
-const _fileContent = r'''diff --git a/.drone.yml b/.drone.yml
+const _file1Content = r'''diff --git a/.drone.yml b/.drone.yml
 index 170b94f65..74cb4811c 100644
 --- a/.drone.yml
 +++ b/.drone.yml
@@ -70,4 +83,19 @@ index 170b94f65..74cb4811c 100644
      commands:
        - dart ci/generate_coverage_helper.dart
        - flutter test --no-pub --coverage -r expanded
+''';
+
+const _file2Content = r'''diff --git a/lib/presentation/widgets/download_all_modal.dart b/lib/presentation/widgets/download_all_modal.dart
+new file mode 100644
+index 000000000..13ebb664b
+--- /dev/null
++++ b/lib/presentation/widgets/download_all_modal.dart
+@@ -0,0 +1,112 @@
++import 'package:flutter/material.dart';
++import 'package:flutter_svg/flutter_svg.dart';
++import 'package:palco/presentation/constants/app_images.dart';
++import 'package:rammstein/presentation/constants/app_svgs.dart';
++import 'package:rammstein/presentation/themes/rammstein_color_scheme.dart';
++import 'package:rammstein/presentation/themes/rammstein_text_theme.dart';
++import 'package:rammstein/shared/file_size.dart';
 ''';
