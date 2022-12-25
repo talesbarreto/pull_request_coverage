e1179cdfc72181ad5811697b3ac7fbc15821b941
Barreto <barreto.tales@gmail.com>
2022-12-25 10:46:22 -0300

    fix tests

--- test/domain/input_reader/diff_reader/use_case/convert_file_diff_from_git_diff_to_file_diff_test.dart
+++ test/domain/input_reader/diff_reader/use_case/convert_file_diff_from_git_diff_to_file_diff_test.dart
@@ -15,8 +15,8 @@ void main() {
       () => expect(result.lines.firstWhere((element) => element.lineNumber == 17).line, "     commands:"),
     );
     test(
-      "line 55 is `+    image: cirrusci/flutter:3.3.7`",
-      () => expect(result.lines.firstWhere((element) => element.lineNumber == 55).line, "+    image: cirrusci/flutter:3.3.7"),
+      "line 55 is `    image: cirrusci/flutter:3.3.7`",
+      () => expect(result.lines.firstWhere((element) => element.lineNumber == 55).line, "    image: cirrusci/flutter:3.3.7"),
     );
   });
 
@@ -29,7 +29,7 @@ void main() {
 
     test(
       "expect that line 17 is `     commands:`",
-      () => expect(result.lines.firstWhere((element) => element.lineNumber == 2).line, "+import 'package:flutter_svg/flutter_svg.dart';"),
+      () => expect(result.lines.firstWhere((element) => element.lineNumber == 2).line, "import 'package:flutter_svg/flutter_svg.dart';"),
     );
   });
 }
