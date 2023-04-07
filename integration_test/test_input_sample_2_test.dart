623ebe65edd0c4521b6171e2273c3439f8c002c0
Tales Barreto <barreto.tales@gmail.com>
2023-04-07 09:37:32 -0300

    integration tests for sample1

--- integration_test/test_input_sample_2_test.dart
+++ integration_test/test_input_sample_2_test.dart
@@ -2,46 +2,45 @@ import 'package:test/test.dart';
 
 import 'shared/integration_test_set.dart';
 
-
 void main() {
-  const lcovFileParam = ["--lcov-file", "input_samples/sample1/lcov.info"];
-  const diffFilePath = "input_samples/sample1/git.diff";
+  const lcovFileParam = ["--lcov-file", "input_samples/sample2/lcov.info"];
+  const diffFilePath = "input_samples/sample2/git.diff";
   group("when running with default settings", () {
-    test("report 89 lines that should be tested", () async {
+    test("report 22 lines that should be tested", () async {
       final analyze = await getAnalyzeForIntegrationTest(
         arguments: lcovFileParam,
         diffFilePath: diffFilePath,
       );
       final result = await analyze();
-      expect(result.linesShouldBeTested, 89);
+      expect(result.linesShouldBeTested, 26);
     });
 
-    test("report 6 lines missing test", () async {
+    test("report 8 lines missing test", () async {
       final analyze = await getAnalyzeForIntegrationTest(
         arguments: lcovFileParam,
         diffFilePath: diffFilePath,
       );
       final result = await analyze();
-      expect(result.linesMissingTests, 6);
+      expect(result.linesMissingTests, 8);
     });
   });
 
   group("When `override` annotation is ignored", () {
-    test("report 89 lines that should be tested", () async {
+    test("report 23 lines that should be tested", () async {
       final analyze = await getAnalyzeForIntegrationTest(
         arguments: [...lcovFileParam, "--ignore-lines", "^.*@override.*\$"],
         diffFilePath: diffFilePath,
       );
       final result = await analyze();
-      expect(result.linesShouldBeTested, 89);
+      expect(result.linesShouldBeTested, 23);
     });
-    test("report 0 untested ignored lines", () async {
+    test("report 3 untested ignored lines", () async {
       final analyze = await getAnalyzeForIntegrationTest(
         arguments: [...lcovFileParam, "--ignore-lines", "^.*@override.*\$"],
         diffFilePath: diffFilePath,
       );
       final result = await analyze();
-      expect(result.untestedIgnoredLines, 0);
+      expect(result.untestedIgnoredLines, 3);
     });
   });
 }
