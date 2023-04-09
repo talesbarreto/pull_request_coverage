132a9399b31ed693eea0e6d0064f4add12485be1
Tales Barreto <barreto.tales@gmail.com>
2023-04-09 10:56:27 -0300

    new output design

--- lib/src/presentation/output_generator/cli_output_generator.dart
+++ lib/src/presentation/output_generator/cli_output_generator.dart
@@ -1,28 +1,13 @@
-import 'package:pull_request_coverage/src/domain/analyzer/models/analysis_result.dart';
-import 'package:pull_request_coverage/src/domain/user_options/models/user_options.dart';
-import 'package:pull_request_coverage/src/presentation/output_generator/get_result_table.dart';
 import 'package:pull_request_coverage/src/presentation/output_generator/plain_text_output_generator.dart';
 import 'package:pull_request_coverage/src/presentation/use_case/colorize_cli_text.dart';
 
-class CliOutputGenerator with PlainTextOutputGenerator {
-  @override
-  final ColorizeCliText colorizeCliText;
-  @override
-  final UserOptions userOptions;
-  final GetResultTable getResultTable;
-
+class CliOutputGenerator extends PlainTextOutputGenerator {
   const CliOutputGenerator({
-    required this.colorizeCliText,
-    required this.userOptions,
-    required this.getResultTable,
-    required this.print,
+    required ColorizeCliText super.colorizeCliText,
+    required super.print,
+    required super.tableBuilder,
+    required super.userOptions,
   });
-
-  @override
-  final void Function(String message) print;
-
-  @override
-  bool get showUncoveredCode => userOptions.showUncoveredCode;
 
   @override
   String? getSourceCodeHeader() => null;
@@ -34,21 +19,28 @@ class CliOutputGenerator with PlainTextOutputGenerator {
   String? getSourceCodeBlocDivider() => "......\n\n";
 
   @override
-  String? getLine(String line, int lineNumber, bool isANewLine, bool isUntested) {
-    if (isANewLine && isUntested) {
-      return "${colorizeCliText("[$lineNumber]: ${line.replaceFirst("+", "→")}", TextColor.red)}\n";
-    } else {
-      return " $lineNumber : $line\n";
-    }
+  String getFileHeader(String filePath, int newLinesCount, int ignoredUntestedLines, int missingTestsLines) {
+    final ignoredText = ignoredUntestedLines > 0
+        ? colorizeText("$ignoredUntestedLines untested and ignored", ColorizeCliText.ignoredUntestedCodeColor)
+        : null;
+    final untestedText =
+        missingTestsLines > 0 ? "${colorizeText("$missingTestsLines lines missing tests", TextColor.red)}\n" : null;
+
+    return "${colorizeText(filePath, missingTestsLines > 0 ? TextColor.yellow : TextColor.noColor)}"
+        " (${colorizeCliText!("+$newLinesCount", TextColor.green)})"
+        "${ignoredText != null || untestedText != null ? "\n ┗━▶" : ""}"
+        "${ignoredText != null ? " $ignoredText" : ""}"
+        "${ignoredText != null && untestedText != null ? ", " : " "}"
+        "${untestedText ?? ""}";
   }
 
   @override
-  String getReport(AnalysisResult analysisResult) {
-    if (analysisResult.linesMissingTests == 0 && userOptions.fullyTestedMessage != null) {
-      return userOptions.fullyTestedMessage!;
+  String? getLine(String line, int lineNumber, bool isANewLine, bool isUntested) {
+    if (isANewLine && isUntested) {
+      return "${colorizeCliText!("[$lineNumber]: ${line.replaceFirst("+", "→")}", TextColor.red)}\n";
+    } else {
+      return " $lineNumber : $line\n";
     }
-
-    return "\n${getResultTable(analysisResult)}";
   }
 
   @override
--- lib/src/presentation/output_generator/plain_text_output_generator.dart
+++ lib/src/presentation/output_generator/plain_text_output_generator.dart
@@ -1,16 +1,31 @@
 import 'dart:math';
 import 'package:pull_request_coverage/src/domain/analyzer/models/analysis_result.dart';
 import 'package:pull_request_coverage/src/domain/input_reader/diff_reader/models/file_diff.dart';
+import 'package:pull_request_coverage/src/domain/user_options/models/output_mode.dart';
 import 'package:pull_request_coverage/src/domain/user_options/models/user_options.dart';
 import 'package:pull_request_coverage/src/presentation/output_generator/output_generator.dart';
+import 'package:pull_request_coverage/src/presentation/output_generator/table_builder.dart';
 import 'package:pull_request_coverage/src/presentation/use_case/colorize_cli_text.dart';
 
-mixin PlainTextOutputGenerator implements OutputGenerator {
-  static const printCodeWindow = 3;
-  void Function(String message) get print;
-  UserOptions get userOptions;
-  bool get showUncoveredCode;
-  ColorizeCliText? get colorizeCliText;
+abstract class PlainTextOutputGenerator implements OutputGenerator {
+  static const printCodeFrame = 3;
+
+  const PlainTextOutputGenerator({
+    required this.tableBuilder,
+    required this.userOptions,
+    required this.colorizeCliText,
+    required this.print,
+  });
+
+  final void Function(String message) print;
+
+  final UserOptions userOptions;
+
+  final ColorizeCliText? colorizeCliText;
+
+  final TableBuilder tableBuilder;
+
+  String getFileHeader(String filePath, int newLinesCount, int ignoredUntestedLines, int missingTestsLines);
 
   String formatFileHeader(String text);
 
@@ -23,8 +38,6 @@ mixin PlainTextOutputGenerator implements OutputGenerator {
 
   String? getSourceCodeFooter();
 
-  String getReport(AnalysisResult analysisResult);
-
   // since [PlainTextOutputGenerator] prints everything on the fly, [printOutput] does nothing
   @override
   void printOutput() {}
@@ -32,41 +45,27 @@ mixin PlainTextOutputGenerator implements OutputGenerator {
   String colorizeText(String text, TextColor color) =>
       colorizeCliText != null ? colorizeCliText!.call(text, color) : text;
 
-  String? getFileHeader(FileDiff fileDiff) {
-    final filePath = fileDiff.path;
-    final uncoveredLinesCount = fileDiff.uncoveredNewLinesCount;
-    final totalNewLinesCount = fileDiff.newLinesCount;
-    final ignoredMsg = fileDiff.ignoredUntestedLinesCount > 0
-        ? " / ${colorizeText("${fileDiff.ignoredUntestedLinesCount} untested and ignored", ColorizeCliText.ignoredUntestedCodeColor)}"
-        : "";
-    if (uncoveredLinesCount == 0) {
-      if (userOptions.reportFullyCoveredFiles) {
-        return "$filePath is fully covered (${colorizeText("+$totalNewLinesCount", TextColor.green)}$ignoredMsg)";
-      }
-      return null;
-    }
-    return "${colorizeText(filePath, TextColor.yellow)}"
-    ": ${colorizeText("$uncoveredLinesCount lines missing tests",TextColor.red)}"
-    " (${colorizeText("+$totalNewLinesCount", TextColor.green)} "
-    "$ignoredMsg)\n";
-  }
-
   @override
   void addFile(FileDiff fileDiff) {
     final outputBuilder = StringBuffer();
     void write(String? message) => message != null ? outputBuilder.write(message) : null;
 
-    write(getFileHeader(fileDiff));
+    write(getFileHeader(
+      fileDiff.path,
+      fileDiff.newLinesCount,
+      fileDiff.ignoredUntestedLinesCount,
+      fileDiff.uncoveredNewLinesCount,
+    ));
     int? lastPrintedLineNumber;
 
-    if (fileDiff.hasUncoveredLines && showUncoveredCode) {
+    if (fileDiff.hasUncoveredLines && userOptions.showUncoveredCode) {
       write(getSourceCodeHeader());
       for (int i = 0; i < fileDiff.lines.length; i++) {
         final line = fileDiff.lines[i];
         var shouldPrint = line.isTestMissing;
         if (!shouldPrint) {
-          // printing [printCodeWindow] codes after and before the uncovered line
-          for (int j = max(i - printCodeWindow, 0); j < fileDiff.lines.length && j < printCodeWindow + i + 1; j++) {
+          // printing [printCodeFrame] codes after and before the uncovered line
+          for (int j = max(i - printCodeFrame, 0); j < fileDiff.lines.length && j < printCodeFrame + i + 1; j++) {
             if (fileDiff.lines[j].isTestMissing) {
               shouldPrint = true;
               break;
@@ -90,6 +89,35 @@ mixin PlainTextOutputGenerator implements OutputGenerator {
 
   @override
   void setReport(AnalysisResult analysisResult) {
-    print(getReport(analysisResult));
+    final maximumUncoveredLines = userOptions.maximumUncoveredLines;
+    final minimumCoverageRate = userOptions.minimumCoverageRate;
+
+    final coverage = (analysisResult.coverageRate * 100);
+    final coverageTxt = coverage.isNaN ? "-" : "${coverage.toStringAsFixed(userOptions.fractionalDigits)}%";
+
+    String result(bool success) =>
+        success ? colorizeText("Success", TextColor.green) : colorizeText("FAIL", TextColor.red);
+
+    final linesResult =
+        maximumUncoveredLines == null ? "-" : result(analysisResult.linesMissingTests <= maximumUncoveredLines);
+    final lineThreshold = maximumUncoveredLines == null ? "-" : "$maximumUncoveredLines";
+    final rateResult =
+        minimumCoverageRate == null ? "-" : result(analysisResult.coverageRate >= (minimumCoverageRate / 100));
+    final rateThreshold = minimumCoverageRate == null ? "-" : "$minimumCoverageRate%";
+
+    final ignoredUntestedLinesText = colorizeText(
+      analysisResult.untestedIgnoredLines.toString(),
+      ColorizeCliText.ignoredUntestedCodeColor,
+    );
+
+    tableBuilder
+      ..setHeader(["Report", "Current value", "Threshold", ""])
+      ..addLine(["Lines that should be tested", analysisResult.linesShouldBeTested.toString(), "", ""])
+      ..addLine(["Ignored untested lines", ignoredUntestedLinesText, "", ""])
+      ..addLine(["", "", "", ""])
+      ..addLine(["Lines missing tests", analysisResult.linesMissingTests.toString(), lineThreshold, linesResult])
+      ..addLine(["Coverage rate", coverageTxt, rateThreshold, rateResult]);
+
+    print("\n${tableBuilder.build(userOptions.outputMode == OutputMode.markdown)}");
   }
 }
